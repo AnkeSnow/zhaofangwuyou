19ce424c9231b25ae9e08f5d09318620f4994cbc
杨茗朝 <1727995491@qq.com>
2023-10-10 13:45:59 +0800

    更新登录页面

---
+++ js/login.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    var switchover = this.document.getElementsByClassName('switchover')[0].children;
+    for (var i = 0; i < switchover.length; i++) {
+        switchover[i].addEventListener('click', function () {
+
+        })
+    }
+})
